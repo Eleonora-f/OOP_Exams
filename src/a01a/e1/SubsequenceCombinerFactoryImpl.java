0a3722f8d73b189e73a8e756e6d8c1ff474dc07a
Eleonora_f <eleonora.falconi@studio.unibo.it>
2024-08-06 23:44:38 +0200

    es2 to fix, es1 done

--- src/a01a/e1/SubsequenceCombinerFactoryImpl.java
+++ src/a01a/e1/SubsequenceCombinerFactoryImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
+//FATTO
 public class SubsequenceCombinerFactoryImpl implements SubsequenceCombinerFactory {
 
     @Override
